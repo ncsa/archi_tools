6c5ad398e9b3a80788aced2ac18c629e71ffa8bb
Donald Petravick <petravic@illinois.edu>
2018-06-01 15:44:35 -0500

    a file to facto DB-speciifc, archimate agnostic code.

---
+++ db.py
@@ -0,0 +1,29 @@
+"""
+This file contains code related common query and databse codes.
+
+This file is for DB-generic support.  e.g. nothing archimate
+or model specific.
+"""
+
+import sqlite3
+import shlog
+import tabulate
+
+
+######################################################
+#
+#  HACK -- copied this  -- need more code factoring.
+#
+########################################################
+
+def q(args, sql):
+    #a funnel routned for report queries, main benefit is query printing
+    con = sqlite3.connect(args.dbfile)
+    con.text_factory = lambda x: x.decode("utf-8")
+    cur = con.cursor()
+    shlog.verbose (sql)
+    result  = cur.execute (sql)
+    con.commit()
+    return result
+
+
