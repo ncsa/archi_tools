65919726924676a81bc3c1c8c578ff3fd4a529b5
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-03-05 09:04:39 -0600

    NEW: * added 'Content' column to the VIEWS_OBJECTS database FIX: * vo.element_id will be used now instead of oiv.object_id

--- archi_tool.py
+++ archi_tool.py
@@ -150,9 +150,9 @@ viewsTable.check()
 #Record ID's that have been folder from CSV's to distinguich from those created.
 viewobjectsTable = SQLTable()
 viewobjectsTable.tableName = 'VIEW_OBJECTS'
-viewobjectsTable.columns   =['View_id','Object_id','Class', 'Name']
-viewobjectsTable.hfm       =[        t,          t,      t,      t]
-viewobjectsTable.hdt       =['text'   ,     'text', 'text', 'text']
+viewobjectsTable.columns   =['View_id','Object_id','Class', 'Name', 'Content']
+viewobjectsTable.hfm       =[        t,          t,      t,      t,         t]
+viewobjectsTable.hdt       =['text'   ,     'text', 'text', 'text',    'text']
 viewobjectsTable.check()
 
 
@@ -408,7 +408,7 @@ def ingest_view_objects(args, sqldbfile):
              INNER JOIN views_objects_in_view voiv on voiv.view_id=dv.view_id AND voiv.view_version=dv.view_version
              )
              /*With the correct view object ids+versions identified, we can retrieve the matches from the views_objects table that has all the properties*/
-             SELECT oiv.view_id, oiv.object_id, vo.class, vo.name
+             SELECT oiv.view_id, vo.element_id, vo.class, vo.name, vo.content
              FROM objects_in_view oiv
              INNER JOIN views_objects vo on vo.container_id=oiv.view_id AND vo.id=oiv.object_id AND vo.version=oiv.object_version
              """ % args.prefix
