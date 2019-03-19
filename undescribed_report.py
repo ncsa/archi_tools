222d61850880ee8271189c661ed580518e27263b
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-03-19 15:59:16 -0500

    CHANGES: undescribed_report.py * reverted accidental erasure of the report

--- undescribed_report.py
+++ undescribed_report.py
@@ -5,9 +5,40 @@ def undescribed_report(args):
 
 
     Folders  = StanzaFactory(args,
-                             """SELECT ID
-                                FROM RELATIONS
-                                WHERE Type = 'AssociationRelationship'"""
+                             """WITH ViewIDs(ID) AS (SELECT r.Source as ID
+                                FROM RELATIONS r
+                                
+                                UNION ALL
+
+                                SELECT r.Target  as ID
+                                FROM RELATIONS r),
+                                
+                                UniqueIDs(ID) AS (SELECT DISTINCT ID 
+                                FROM ViewIDs
+                                ),
+                                
+                                ElementIDs (Total, Undescribed) AS (SELECT 
+                                count(e.ID) as Total,
+                                SUM(CASE e.Documentation
+                                    WHEN NULL THEN 1
+                                    WHEN '' THEN 1
+                                    ELSE 0
+                                END) as Undescribed
+                                FROM ELEMENTS e)
+                                
+                                SELECT 'All Elements' as OutputType, * , '=D2/C2' as PercentUndescribed
+                                FROM ElementIDs
+                                
+                                UNION ALL
+                                
+                                SELECT 'Elements used in views' as OutputType, count(e.ID) as Total,
+                                SUM(CASE e.Documentation
+                                    WHEN NULL THEN 1
+                                    WHEN '' THEN 1
+                                    ELSE 0
+                                END) as Undescribed, '=D3/C3' as PercentUndescribed
+                                FROM ELEMENTS e
+                                INNER JOIN UniqueIDs u on u.ID=e.ID"""
     )
     Folders.add_report_segment(
         SegmentSQL("SELECT '{OutputType}' as OutputType, '{Total}' as Total, '{Undescribed}' as Undescribed, '{PercentUndescribed}' as PercentUndescribed ")
