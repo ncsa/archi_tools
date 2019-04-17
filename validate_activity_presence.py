92b76e909235c3b427cbe3dc33dcd5bc2ae564c7
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-04-17 15:27:47 -0500

    CHANGES validate_application_presence.py * renamed to validate_activity_presence.py * reworked SQL to be more efficient * added view info to output

---
+++ validate_activity_presence.py
@@ -0,0 +1,30 @@
+from reports import *
+
+def validate_activity_presence(args):
+
+
+    Elem  = StanzaFactory(args,
+                             """WITH AllViewActivity(ElementId, ViewId, Depth) AS (SELECT DISTINCT e.Id as ElementId, v.Id as ViewId, f.Depth
+                                FROM FOLDER f
+                                INNER JOIN VIEWS v on v.Parent_folder_id = f.Id
+                                INNER JOIN VIEW_OBJECTS vo on vo.view_id = v.Id
+                                INNER JOIN ELEMENTS e on e.Id = vo.Object_id
+                                WHERE e.Type like '%Function' or e.Type like '%Process' or e.Type like '%Process')
+                                SELECT ElementId, ViewId
+                                FROM AllViewActivity 
+                                WHERE Depth like '%Service Realization Viewpoint%'
+                                AND ElementId not in (SELECT ElementId FROM AllViewActivity WHERE Depth like '%Behavior to Provisioning Map Viewpoint%')"""
+    )
+    Elem.add_report_segment(
+        SegmentSQL("""SELECT Name, Type, Documentation from ELEMENTS WHERE Id = '{ElementId}' """)
+    )
+    Elem.add_report_segment(
+        SegmentSQL("""SELECT Name, Documentation from VIEWS WHERE Id = '{ViewId}' """)
+    )
+
+
+
+
+
+    # Elem.report({})
+    return Elem
