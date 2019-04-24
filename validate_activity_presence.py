5e29fe6152a5ad9df8db41ff712818b6d810cfc1
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-04-24 10:45:57 -0500

    CHNAGES: validate_activity_presence.py * fixed some items not showing up

--- validate_activity_presence.py
+++ validate_activity_presence.py
@@ -2,14 +2,15 @@ from reports import *
 
 def validate_activity_presence(args):
 
-    # This validation checks for active aka "round" elements present in Service Realization Viewpoint but not in Behavior to Provisioning Maps
+    # This validation checks for active aka "round" elements present in Service Realization Viewpoint
+    # but not in Behavior to Provisioning Maps
     Elem  = StanzaFactory(args,
                              """WITH AllViewActivity(ElementId, ViewId, Depth) AS (SELECT DISTINCT e.Id as ElementId, v.Id as ViewId, f.Depth
                                 FROM FOLDER f
                                 INNER JOIN VIEWS v on v.Parent_folder_id = f.Id
                                 INNER JOIN VIEW_OBJECTS vo on vo.view_id = v.Id
                                 INNER JOIN ELEMENTS e on e.Id = vo.Object_id
-                                WHERE e.Type like '%Function' or e.Type like '%Process' or e.Type like '%Process')
+                                WHERE e.Type like '%Function' or e.Type like '%Process' or e.Type like '%Process' or e.Type like '%Service')
                                 SELECT ElementId, ViewId
                                 FROM AllViewActivity 
                                 WHERE Depth like '%Service Realization Viewpoint%'
