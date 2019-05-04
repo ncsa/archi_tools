cba6593b3abb5d11f40152ae3a0d60c9c818e1bf
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-05-03 22:43:06 -0500

    CHANGES: list_modules.py * added ez execute mode * changed some output lines * finished verbose mode

--- list_modules.py
+++ list_modules.py
@@ -1,12 +1,14 @@
 import os
-import shlog
+import glob
+import re
 import argparse
+import shlog
 
-def show_comments(dir, file):
-    pyFile = open(dir + file, 'r')
+def show_comments(file):
+    pyFile = open(file, 'r')
     for line in pyFile:
         if '# ' in line:
-            shlog.normal(line.replace('\n', ''))
+            print(line.replace('\n', ''))
     pyFile.close()
 
 if __name__ == "__main__":
@@ -15,29 +17,49 @@ if __name__ == "__main__":
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     main_parser.add_argument('--loglevel', '-l',
-                             help=shlog.LOGHELP,
-                             default="NORMAL")
+                             help='loglevel NONE, NORMAL, VERBOSE, VVERBOSE, DEBUG',
+                             default="ERROR")
+    main_parser.add_argument('--ezrun', '-ez', default=None,
+                             help='execute reports.py with -l VERBOSE report -s -ce REPORT_NAME flags.\nUse: r0 to run 0th report, v3 to run 3rd validation, etc.')
     args = main_parser.parse_args()
 
-    # translate text arguement to log level.
-    # least to most verbose FATAL WARN INFO DEBUG
-    # level also printst things to the left of it.
+    # init the logger
     loglevel = shlog.__dict__[args.loglevel]
     assert type(loglevel) == type(1)
     shlog.basicConfig(level=shlog.__dict__[args.loglevel])
 
+    # ez execute mode
+    if args.ezrun != None:
+        if re.match(r'(r|v)([0-9]+)', args.ezrun):
+            if args.ezrun.startswith('r'):
+                fileList = glob.glob('report/*.py')
+            else:
+                fileList = glob.glob('validate/*.py')
+            try:
+                targetPy = fileList[int(args.ezrun[1:])]
+                targetPy = targetPy.replace('.py','')
+                targetPy = 'python reports.py -l VERBOSE report -s -ce ' + targetPy
+                shlog.verbose('Issuing term command: ' + targetPy)
+                os.system(targetPy)
+                shlog.verbose(targetPy + ' executed')
+            except IndexError:
+                print('Invalid module #')
+        else:
+            print("--ezrun " + args.ezrun + " parameter is not valid. It should start with a lowercase 'r' or 'v' and a corresponding module #")
+        exit(0)
+
+    # normal mode: list all modules
     i = 0
-    shlog.normal('AVAILABLE REPORTS')
-    for file in os.listdir('report/'):
-        if file.endswith('.py'):
-            shlog.normal('#' +str(i) + ' ' + file)
-            show_comments('report/', file)
-            i += 1
+    print('#_______________#\nAVAILABLE REPORTS')
+    for file in glob.glob('report/*.py'):
+        print('#' +str(i) + ' ' + file)
+        show_comments(file)
+        i += 1
 
     i = 0
-    shlog.normal('AVAILABLE VALIDATIONS')
-    for file in os.listdir('validate/'):
-        if file.endswith('.py'):
-            shlog.normal('#' + str(i) + ' ' + file)
-            show_comments('validate/', file)
-            i += 1
+    print('#_______________#\nAVAILABLE VALIDATIONS')
+    for file in glob.glob('validate/*.py'):
+        print('#' + str(i) + ' ' + file)
+        show_comments(file)
+        i += 1
+    print('\nUse the -ez parameter to quickly run a report\n')
